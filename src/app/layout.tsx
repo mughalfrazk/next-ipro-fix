fd879080d0efcf85284c3b8ab36ff2be055ac99b
Faraz Khalil <mughalfrazk@gmail.com>
2024-09-10 00:30:04 +0100

    feat(refactor): project setup and cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,16 +7,16 @@ import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata = {
   title: 'My Mantine app',
