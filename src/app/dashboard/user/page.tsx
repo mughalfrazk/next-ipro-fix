b633f5e5819f732466c9ef86061cc9d1716c6eb0
Faraz Mughal <mughalfrazk@gmail.com>
2024-10-29 14:46:43 +0000

    chore: refactor

--- src/app/dashboard/user/page.tsx
+++ src/app/dashboard/user/page.tsx
@@ -1,9 +1,11 @@
 import { getUserListApi } from "@/lib/services/api/user.service";
-import UserList from "@/components/user/UserTable";
+import UserList from "@/components/user/UserList";
 
 const UsersPage = async () => {
   const result = await getUserListApi();
+  console.log(result);
   return <UserList users={result} />;
 };
 
 export default UsersPage;
+
--- src/components/user/user-columns.tsx
+++ src/components/user/user-columns.tsx
@@ -1,124 +1,171 @@
-"use client"
+"use client";
 
-import { Avatar, Badge, Button, Group, Stack, Text, Title } from "@mantine/core";
-import { ProfileModel, UserModel} from "@/lib/models/user.model";
+import {
+  Avatar,
+  ProgressLabel,
+  ProgressRoot,
+  ProgressSection,
+  Badge,
+  Button,
+  Group,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
+import { ProfileModel, UserModel } from "@/lib/models/user.model";
 import { redirect } from "next/navigation";
 import { colorForUserRole } from "@/utils/functions";
 
 export const createNewJobHandler = () => {
-    redirect("/dashboard/user/add-new")
-  }
+  redirect("/dashboard/user/add-new");
+};
 
-  export const UserColumns = [
-    {
-      accessor: "id",
-    },
-    {
-      accessor: "user",
-      render: (row:UserModel) => {
-        return (
-          <Group>
-            <Avatar
-              src="../../assets/Avatar_man.png" alt="user_image" 
-            />
-            <Stack gap={2}>
-            <Title
-              order={6}
-            >{`${row.first_name} ${row.last_name}`}</Title>
+export const UserColumns = [
+  {
+    accessor: "id",
+  },
+  {
+    accessor: "user",
+    render: (row: UserModel) => {
+      return (
+        <Group>
+          <Avatar src="../../assets/Avatar_man.png" alt="user_image" />
+          <Stack gap={2}>
+            <Title order={6}>{`${row.first_name} ${row.last_name}`}</Title>
             <Text size="sm">{`${row.email}`}</Text>
-            </Stack>
-          </Group>
-        );
-      },
+          </Stack>
+        </Group>
+      );
     },
+  },
 
-    // {
-    //   accessor: "role",
-    //   render: (row: ProfileModel) => {
-    //     return (
-    //       <Badge variant="outline" color={colorForUserRole(row.role.name)} radius="sm" p={12}>
-    //         {row.role.name}
-    //       </Badge>
-    //     );
-    //  },
-    // },
-    
-    {
-      accessor: "phone",
+  {
+    accessor: "role",
+    render: (row: ProfileModel) => {
+      return (
+        <Badge
+          variant="outline"
+          color={colorForUserRole(row.role.name)}
+          radius="sm"
+          p={12}
+        >
+          {row.role.name}
+        </Badge>
+      );
     },
-    
-    {
-      accessor: "jobId",
-      render: (row:UserModel) => {
-        return <Button variant="white" size="compact-sm" color="var(--mantine-color-primary-6)">{"Open User"}</Button>;
-      },
-    },
+  },
 
-    // {
-    //   accessor: "user",
-    //   title: "User",
-    //   render: (row: ProfileModel) => {
-    //     return (
-    //       <Group>
-    //         <Avatar
-    //           src="../../assets/Avatar_man.png" alt="user_image" 
-    //         />
-    //         <Stack gap={2}>
-    //         <Title
-    //           order={6}
-    //         >{`${row.user.first_name} ${row.user.last_name}`}</Title>
-    //           <Text size={"0.7rem"}>{row.user.email}</Text>
-    //         </Stack>
-    //       </Group>
-    //     );
-    //   },
-    // },
-    // {
-    //   accessor: "role",
-    //   render: (row: ProfileModel) => {
-    //     return (
-    //       <Badge variant="outline" color={colorForUserRole(row.role.name)} radius="sm" p={12}>
-    //         {row.role.name}
-    //       </Badge>
-    //     );
-    //   },
-    // },
-    // {
-    //   accessor: "quantity",
-    //   render: (row: ProfileModel) => {
-    //     return row.issues.reduce((prev, curr) => prev + curr.quantity, 0);
-    //   },
-    // },
-    // {
-    //   title: "Company Name",
-    //   accessor: "customer.company_name",
-    // },
-    // {
-    //   accessor: "technician",
-    //   render: (row: JobModel) => {
-    //     return (
-    //       <Group>
-    //         <Avatar
-    //           key={row.technician.id}
-    //           name={`${row.technician.first_name} ${row.technician.last_name}`}
-    //           color="initials"
-    //         />
-    //         <Stack gap={2}>
-    //           <Title
-    //             order={6}
-    //           >{`${row.technician.first_name} ${row.technician.last_name}`}</Title>
-    //           <Text
-    //             size={"0.7rem"}
-    //           >{`${row.technician.first_name} ${row.technician.last_name}`}</Text>
-    //         </Stack>
-    //       </Group>
-    //     );
-    //   },
-    // },
-    // {
-    //   accessor: "total",
-    //   render: (row: JobModel) => {
-    //     return row.issues.reduce((prev, curr) => prev + curr.total, 0);
-    //   },
-    // },
-  ];
+  {
+    accessor: "target",
+    render: (row: UserModel) => {
+      const progressPercentage = (
+        ((row.progress ?? 0) / (row.target ?? 1)) *
+        100
+      ).toFixed(2);
+      console.log("Progress Percentage:", progressPercentage);
+      return (
+        <ProgressRoot size="xl" w={150}>
+          console.log((row.progress ?? 0) / (row.target ?? 0) * 100))
+          <ProgressSection value={row.target ?? 0} color="primary.6">
+            <ProgressLabel>
+              {((row.progress ?? 0) / (row.target ?? 0)) * 100}%
+            </ProgressLabel>
+          </ProgressSection>
+        </ProgressRoot>
+      );
+      // return <Progress color="var(--mantine-color-primary-6)" radius="xl" value={row.target ?? 0} striped animated />;
+    },
+  },
+
+  {
+    accessor: "progress",
+    title: "Earned Amount",
+  },
+
+  {
+    accessor: "phone",
+  },
+
+  {
+    accessor: "actions",
+    render: (row: UserModel) => {
+      return (
+        <Button
+          variant="transparent"
+          size="compact-sm"
+          color="var(--mantine-color-primary-6)"
+        >
+          {"Open User"}
+        </Button>
+      );
+    },
+  },
+
+  // {
+  //   accessor: "user",
+  //   title: "User",
+  //   render: (row: ProfileModel) => {
+  //     return (
+  //       <Group>
+  //         <Avatar
+  //           src="../../assets/Avatar_man.png" alt="user_image"
+  //         />
+  //         <Stack gap={2}>
+  //         <Title
+  //           order={6}
+  //         >{`${row.user.first_name} ${row.user.last_name}`}</Title>
+  //           <Text size={"0.7rem"}>{row.user.email}</Text>
+  //         </Stack>
+  //       </Group>
+  //     );
+  //   },
+  // },
+  // {
+  //   accessor: "role",
+  //   render: (row: ProfileModel) => {
+  //     return (
+  //       <Badge variant="outline" color={colorForUserRole(row.role.name)} radius="sm" p={12}>
+  //         {row.role.name}
+  //       </Badge>
+  //     );
+  //   },
+  // },
+  // {
+  //   accessor: "quantity",
+  //   render: (row: ProfileModel) => {
+  //     return row.issues.reduce((prev, curr) => prev + curr.quantity, 0);
+  //   },
+  // },
+  // {
+  //   title: "Company Name",
+  //   accessor: "customer.company_name",
+  // },
+  // {
+  //   accessor: "technician",
+  //   render: (row: JobModel) => {
+  //     return (
+  //       <Group>
+  //         <Avatar
+  //           key={row.technician.id}
+  //           name={`${row.technician.first_name} ${row.technician.last_name}`}
+  //           color="initials"
+  //         />
+  //         <Stack gap={2}>
+  //           <Title
+  //             order={6}
+  //           >{`${row.technician.first_name} ${row.technician.last_name}`}</Title>
+  //           <Text
+  //             size={"0.7rem"}
+  //           >{`${row.technician.first_name} ${row.technician.last_name}`}</Text>
+  //         </Stack>
+  //       </Group>
+  //     );
+  //   },
+  // },
+  // {
+  //   accessor: "total",
+  //   render: (row: JobModel) => {
+  //     return row.issues.reduce((prev, curr) => prev + curr.total, 0);
+  //   },
+  // },
+];
