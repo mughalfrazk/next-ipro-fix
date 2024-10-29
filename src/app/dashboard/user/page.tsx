6b96a819884bdd8c6fcd1be58e2899c1e3743eb5
IproFixGH <mughalfrazk@gmail.com>
2024-10-29 14:59:00 +0000

    chore: file rename

--- src/app/dashboard/user/page.tsx
+++ src/app/dashboard/user/page.tsx
@@ -1,5 +1,5 @@
 import { getUserListApi } from "@/lib/services/api/user.service";
-import UserList from "@/components/user/UserList";
+import UserList from "@/components/user/UserTable";
 
 const UsersPage = async () => {
   const result = await getUserListApi();
