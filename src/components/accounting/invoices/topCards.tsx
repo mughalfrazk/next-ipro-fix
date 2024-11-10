96147736aeaac2633720d71574922a7acbc947e9
Ali Nawaz <alinawazdoger1996@gmail.com>
2024-11-10 21:10:24 +0500

    chore: formted

--- src/components/accounting/invoices/topCards.tsx
+++ src/components/accounting/invoices/topCards.tsx
@@ -1,18 +1,19 @@
 "use client";
 
 import { Avatar, Grid, Group, Stack, Text, Card } from "@mantine/core";
-import { IconInvoice,IconMoneybag,IconClockRecord,IconCheckbox } from "@tabler/icons-react";
+import {
+  IconInvoice,
+  IconMoneybag,
+  IconClockRecord,
+  IconCheckbox
+} from "@tabler/icons-react";
 const TopCards = () => {
   return (
     <Grid>
       <Grid.Col span={3}>
         <Card>
           <Group>
-            <Avatar
-              bg="violet.9"
-              size="4rem"
-              radius="xl"
-            >
+            <Avatar bg="violet.9" size="4rem" radius="xl">
               <IconInvoice stroke={1.2} color="white" size="1.9rem" />
             </Avatar>
             <Stack gap={0}>
@@ -29,16 +30,12 @@ const TopCards = () => {
       <Grid.Col span={3}>
         <Card>
           <Group>
-            <Avatar
-              bg="cyan.6"
-              size="4rem"
-              radius="xl"
-            >
+            <Avatar bg="cyan.6" size="4rem" radius="xl">
               <IconMoneybag stroke={1.2} color="white" size="1.9rem" />
             </Avatar>
             <Stack gap={0}>
               <Text size="md" c="gray.6">
-              Total Amount
+                Total Amount
               </Text>
               <Text c="cyan.6" size="xl" fw={600}>
                 AED 556.523
@@ -50,16 +47,12 @@ const TopCards = () => {
       <Grid.Col span={3}>
         <Card>
           <Group>
-            <Avatar
-              bg="teal.6"
-              size="4rem"
-              radius="xl"
-            >
+            <Avatar bg="teal.6" size="4rem" radius="xl">
               <IconClockRecord stroke={1.2} color="white" size="1.9rem" />
             </Avatar>
             <Stack gap={0}>
               <Text size="md" c="gray.6">
-              Total Pending
+                Total Pending
               </Text>
               <Text c="teal.6" size="xl" fw={600}>
                 AED 556.523
@@ -71,16 +64,12 @@ const TopCards = () => {
       <Grid.Col span={3}>
         <Card>
           <Group>
-            <Avatar
-              bg="blue.6"
-              size="4rem"
-              radius="xl"
-            >
+            <Avatar bg="blue.6" size="4rem" radius="xl">
               <IconCheckbox stroke={1.2} color="white" size="1.9rem" />
             </Avatar>
             <Stack gap={0}>
               <Text size="md" c="gray.6">
-              Total Paid
+                Total Paid
               </Text>
               <Text c="blue.6" size="xl" fw={600}>
                 AED 556.523
